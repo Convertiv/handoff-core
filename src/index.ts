2f97c1e92876f5e78cc5ed60eff96b516bf44aa7
Domagoj Gojak <domagoj@convertiv.com>
2025-05-20 21:28:44 +0200

    Resolve error TS2550: Property ‘fromEntries’ does not exist on type ‘ObjectConstructor’.

--- src/index.ts
+++ src/index.ts
@@ -22,25 +22,25 @@ const normalizeConfiguration = (
   const sharedTransformerOptions = baseOptions.transformer["*"];
 
   // Build normalized transformer settings
-  const normalizedTransformersOptions = Object.fromEntries(
-    Object.entries(baseOptions.transformer).map(([key, specificOptions]) => {
-      return [
-        key,
-        {
-          cssRootClass: specificOptions.cssRootClass ?? null,
-          tokenNameSegments: specificOptions.tokenNameSegments ?? null,
-          defaults: toLowerCaseKeysAndValues({
-            ...(sharedTransformerOptions?.defaults ?? {}),
-            ...specificOptions.defaults,
-          }),
-          replace: toLowerCaseKeysAndValues({
-            ...(sharedTransformerOptions?.replace ?? {}),
-            ...specificOptions.replace,
-          }),
-        },
-      ];
-    })
-  );
+  const normalizedTransformersOptions = Object.keys(baseOptions.transformer).reduce(function (acc, key) {
+    const specificOptions = baseOptions.transformer[key];
+
+    const sharedDefaults = (sharedTransformerOptions && sharedTransformerOptions.defaults) || {};
+    const sharedReplace = (sharedTransformerOptions && sharedTransformerOptions.replace) || {};
+
+    acc[key] = {
+      cssRootClass: specificOptions.cssRootClass || null,
+      tokenNameSegments: specificOptions.tokenNameSegments || null,
+      defaults: toLowerCaseKeysAndValues(
+        Object.assign({}, sharedDefaults, specificOptions.defaults)
+      ),
+      replace: toLowerCaseKeysAndValues(
+        Object.assign({}, sharedReplace, specificOptions.replace)
+      )
+    };
+
+    return acc;
+  }, {});
 
   return {
     ...configuration,
